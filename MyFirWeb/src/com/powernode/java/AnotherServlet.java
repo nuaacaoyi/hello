386aeb4c85ce0e275482723d34ea0e78c5364098
CY <371779344@qq.com>
2020-02-25 16:41:20 +0800

    servlet应用第二天

---
+++ MyFirWeb/src/com/powernode/java/AnotherServlet.java
@@ -0,0 +1,45 @@
+package com.powernode.java;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.Enumeration;
+
+public class AnotherServlet  implements Servlet {
+    private ServletConfig conf;
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        this.conf=config;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return conf;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        //获取servletContext对象
+        ServletContext sc=conf.getServletContext();
+        Enumeration<String> es= sc.getInitParameterNames();//获取的是整个应用的公共属性
+        while (es.hasMoreElements()){
+            String name=es.nextElement();
+            System.out.println(name+"-->"+sc.getInitParameter(name));
+
+        }
+        //设置域属性  也是全局性变量 可以放到别的servlet中访问
+        sc.setAttribute("email","cayi@mxsvc.cn");
+
+
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
--- MyFirWeb/src/com/powernode/java/MyFirServlet.java
+++ MyFirWeb/src/com/powernode/java/MyFirServlet.java
@@ -18,6 +18,7 @@ public class MyFirServlet implements Servlet {
     public void init(ServletConfig servletConfig) throws ServletException {
         this.conf=servletConfig;
         System.out.println("servletConfig" +servletConfig);
+        //ServletConfig对象其实就是参数配置页
         //servletConfigorg.apache.catalina.core.StandardWrapperFacade
     }
 
@@ -31,17 +32,21 @@ public class MyFirServlet implements Servlet {
         //获取Servlet的名称
         String servletName=conf.getServletName();
         System.out.println("servletName: "+servletName);
-
+        //获取servletContext对象
         ServletContext sc=conf.getServletContext();//单例，一个应用就一个对象
         System.out.println("servletContext: "+conf.getServletContext());
 
         System.out.println("InitParameter: "+conf.getInitParameter("name"));
-
+        //获取所有的初始化参数   枚举
         Enumeration<String> es= conf.getInitParameterNames();
         while (es.hasMoreElements()) {
             String name=es.nextElement();
+            //获取指定参数名对应的参数值
             System.out.println(name+"-->"+conf.getInitParameter(name));
         }
+        //获取AnotherServlet中设置的全局域属性变量
+        System.out.println("email -> " +sc.getAttribute("email"));//再次重申ServletContext属于整个应用级的对象
+        //同样，在这个servlet中还可以重置或者删除域属性
 
     }
 
